b41fbaee173dc07fb4fd5cb5f7a56af0631fa20c
assilbekov <askhat.assilbekov@gmail.com>
2024-05-30 19:54:56 -0300

    refactor TestServerWithMultiClients test

--- client/client_test.go
+++ client/client_test.go
@@ -4,52 +4,9 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"sync"
 	"testing"
 	"time"
 )
-
-func TestNewClients(t *testing.T) {
-	nClients := 10
-	wg := sync.WaitGroup{}
-	wg.Add(nClients)
-
-	for i := 0; i < nClients; i++ {
-		i := i
-		go func() {
-			defer wg.Done()
-			c, err := NewClient("localhost:5001")
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			defer func(c *Client) {
-				err := c.Close()
-				if err != nil {
-					log.Fatal(err)
-				}
-			}(c)
-
-			key := fmt.Sprintf("client_key_%d", i)
-			value := fmt.Sprintf("client_val_%d", i)
-			if err := c.Set(
-				context.Background(),
-				key,
-				value,
-			); err != nil {
-				log.Fatal(err)
-			}
-
-			val, err := c.Get(context.Background(), key)
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Printf("GET val => %s, FROM client => %s\n", val, key)
-		}()
-	}
-
-	wg.Wait()
-}
 
 func TestNewClient(t *testing.T) {
 	c, err := NewClient("localhost:5001")
---
+++ server_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"redis-server-clone-practice/client"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestServerWithMultiClients(t *testing.T) {
+	go func() {
+		server := NewServer(Config{})
+		log.Fatal(server.Start())
+	}()
+
+	time.Sleep(time.Second)
+
+	nClients := 10
+	wg := sync.WaitGroup{}
+	wg.Add(nClients)
+
+	for i := 0; i < nClients; i++ {
+		i := i
+		go func() {
+			defer wg.Done()
+			c, err := client.NewClient("localhost:5001")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			defer func(c *client.Client) {
+				err := c.Close()
+				if err != nil {
+					log.Fatal(err)
+				}
+			}(c)
+
+			key := fmt.Sprintf("client_key_%d", i)
+			value := fmt.Sprintf("client_val_%d", i)
+			if err := c.Set(
+				context.Background(),
+				key,
+				value,
+			); err != nil {
+				log.Fatal(err)
+			}
+
+			val, err := c.Get(context.Background(), key)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("GET val => %s, FROM client => %s\n", val, key)
+		}()
+	}
+
+	wg.Wait()
+}
